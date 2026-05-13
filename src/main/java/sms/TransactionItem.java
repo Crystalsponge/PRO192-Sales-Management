06b6041d96434129070b2f59c15c374aec9f95c7
Crystalsponge <kietluuhoanganh@gmail.com>
2026-05-13 12:36:08 +0700

    Initial base classes creation

---
+++ src/main/java/sms/TransactionItem.java
@@ -0,0 +1,16 @@
+package sms;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LEGION
+ */
+
+//This class is for storing product items inside a transaction
+public class TransactionItem {
+    
+}
