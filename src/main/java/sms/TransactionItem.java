3679c8f6df1f107593d76ebda0caa18728d79d8a
walkernguyen19042007 <walkernguyen19042007@gmail.com>
2026-05-21 14:31:51 +0000

    Update get,set,cons for Transaction Item

--- src/main/java/sms/TransactionItem.java
+++ src/main/java/sms/TransactionItem.java
@@ -15,5 +15,64 @@ public class TransactionItem {
     private Product product;
     private int quantity;
     private double unitPrice;
+
+    public TransactionItem(int quantity,Product product){
+        setgetProduct(product);
+        setQuantity(quantity);
+        
+        //tranh truong hop user quen input product
+        if(product != null){
+            this.unitPrice = product.getPrice();
+            //tra lai gia tri product o class product
+        }
+    }
+
+    //Total amount of a product
+    public double getLineTotal(){
+        return this.unitPrice * this.quantity;
+    }
+
+    //GETTER
+
+    public Product getProduct(){
+        return this.product;
+    }
+
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public double getUnitPrice(){
+        return this.unitPrice;
+    }
+
+    //SETTER 
+
+    public void setProduct(Product product){
+        if(product == null){
+            System.out.println("The product is Invalid! (none)");
+        }
+        else{
+            this.product = product;
+        }
+    }
+
+    public void setQuantity(int quantity){
+        if(quantity < 0){
+            System.out.println("Quantity is Invalid! (<0)");
+        }
+        else{
+            this.quantity = quantity;
+        }
+    }
+
+    public void setUnitPrice(double unitPrice){
+        if(unitPrice < 0){
+            System.out.println("Unit price is Invalid! (<0)");
+        }
+        else{
+            this.unitPrice = unitPrice;
+        }
+    }
     
 }
