8782fc4bb33c22ab547434377a7547ec714a6c18
ThnDyu <thanhduy28092007@gmail.com>
2026-05-23 15:03:05 +0700

    Update Report field,cons,get/setter

--- src/main/java/sms/Report.java
+++ src/main/java/sms/Report.java
@@ -4,10 +4,37 @@
  */
 package sms;
 
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  *
  * @author LEGION
  */
 public class Report {
+    //field
+    private List<Transaction> transactions;
+    private LocalDate date;
+
+    public Report(List<Transaction> transactions, LocalDate date) {
+        this.transactions = transactions;
+        this.date = date;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
     
 }
