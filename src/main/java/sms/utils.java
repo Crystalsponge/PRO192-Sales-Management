4801a88842c8ed75876b4209d07fc18000f22793
Crystalsponge <kietluuhoanganh@gmail.com>
2026-06-02 14:52:16 +0700

    Update validation methods in utils file

--- src/main/java/sms/utils.java
+++ src/main/java/sms/utils.java
@@ -6,5 +6,41 @@ package sms;
 
 //Mainly validation methods and menu UI
 public class utils {
-    
+    public static boolean notEmpty(String s) {
+        if (s == null || s.trim().isEmpty()) {
+            System.out.println("String cannot be empty!");
+            return false;
+        }
+        return true;
+    }
+    public static boolean notNull(Object object) {
+        if(object == null) {
+            System.out.println("Object cannot be null!");
+            return false;
+        }
+        return true;
+    }
+    public static boolean validateName(String s) {
+        if(!notEmpty(s)) {
+            System.out.println("Name cannot be empty");
+            return false;
+        }
+        if(s.matches(".*\\d.*")) {
+            System.out.println("Name cannot contain number");
+            return false;
+        }
+        return true;
+    }
+    public static boolean validatePhone(String s) {
+        if(!notEmpty(s)) {
+            System.out.println("Phone cannot be empty");
+            return false;
+        }
+        if(!s.matches("0\\d{9}")) {
+            System.out.println("Phone number must be 10 digits");
+            return false;
+        }
+        return true;
+    }
 }
+
